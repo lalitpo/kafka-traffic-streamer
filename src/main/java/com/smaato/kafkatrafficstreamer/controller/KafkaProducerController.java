6993528b0db84b6b9695e05f2dd8fe9da52aae18
Lalit Poddar <lalit-kumar.poddar@uni-konstanz.de>
2022-07-14 15:50:29 +0200

    try catch block for response generation

--- src/main/java/com/smaato/kafkatrafficstreamer/controller/KafkaProducerController.java
+++ src/main/java/com/smaato/kafkatrafficstreamer/controller/KafkaProducerController.java
@@ -1,7 +1,10 @@
 package com.smaato.kafkatrafficstreamer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +21,17 @@ public class KafkaProducerController {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @PostMapping(value = "/publish")
-    public void publishMessage(@RequestParam("count") String count) {
-        this.kafkaTemplate.send("trafficCount", count);
+    @GetMapping(value = "/publish")
+    public ResponseEntity<String> publishMessage(@RequestParam("count") String count) {
+        try {
+            this.kafkaTemplate.send("trafficCount", count);
+
+            
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Exception in Kafka", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<String>("Successfull in Kafka", HttpStatus.OK);
+
     }
 
 }
